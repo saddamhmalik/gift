308c2e4739126b17b6aa931c8be53bfcf90eba95
Saddam Hussain <saddamhmalik@gmail.com>
2026-02-28 23:30:51 +0530

    Update AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -9,8 +9,8 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        $email = 'admin@admin.com';
-        $password = bcrypt('password');
+        $email = 'admin@payflex.com';
+        $password = bcrypt('Password@1');
 
         Admin::updateOrCreate(
             ['email' => $email],
